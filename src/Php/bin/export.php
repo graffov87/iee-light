8a1df956d8d04b94628d8234225c786d692092d2
charnou <graffov87@gmail.com>
2018-04-19 20:05:29 +0300

    add code

--- src/Php/bin/export.php
+++ src/Php/bin/export.php
@@ -7,24 +7,36 @@
 
 declare(strict_types=1);
 
-const ENTITY = 'catalog_product';
-
 function includeIfExists($file):void
 {
     if (file_exists($file)) {
         include $file;
     }
 }
+$config = '../../config/';
+$model = '/../Model/';
 
 includeIfExists(__DIR__.'/../../../../../vendor/autoload.php');
 includeIfExists(__DIR__.'/../../../../../autoload.php');
-includeIfExists(__DIR__ .'/../Model/Traits/Additional.php');
-includeIfExists(__DIR__.'/../Model/AbstractDirection.php');
-includeIfExists(__DIR__.'/../Model/Writer/Csv.php');
-includeIfExists(__DIR__.'/../Model/Export.php');
-includeIfExists(__DIR__.'/../Model/Log.php');
-$params =  include __DIR__ .'/../../../../../../app/etc/env.php';
+includeIfExists(__DIR__ . $model . 'Traits/Additional.php');
+includeIfExists(__DIR__. $model . 'AbstractDirection.php');
+includeIfExists(__DIR__. $model . 'Writer/Csv.php');
+includeIfExists(__DIR__. $model . 'Export.php');
+includeIfExists(__DIR__. $model . 'Log.php');
+//$params =  include __DIR__ .'/../../../../../../app/etc/env.php';
+$params = null;
+$text = file_get_contents($config. 'config.json');
+$platforms = json_decode($text, true);
+$paramPlatofms = $platforms['platforms']['Magento2'];
+if (isset($paramPlatofms) && !empty($paramPlatofms)) {
+  $options = json_decode(file_get_contents($config . $paramPlatofms['file']), true);
+  if ($options['path_config']) {
+      $params =include __DIR__ .$options['path_config'];
+  }
+  if (isset($options['mysql'])) {
 
+  }
+}
 $log = new \Chervit\Iee\Log();
 $log->setBeginTime();
 $export = new \Chervit\Iee\Export($params, $log);
